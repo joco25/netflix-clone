1abc44cd19e370d3c0ec73f00ec636866ad72af6
jocosge <onungwej@gmail.com>
2023-10-05 11:49:19 -0400

    completed base for netflix core

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,12 @@ import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
 
 import Navbar from '@/components/Navbar';
+import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
+import useMoviesList from '@/hooks/useMovieList';
+import useFavorites from '@/hooks/useFavorites';
+import InfoModal from '@/components/InfoModal';
+import useInfoModal from '@/hooks/useInfoModal';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -19,9 +25,18 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const { data: movies = [] } = useMoviesList();
+  const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
+      <Billboard />
+      <div className='pb-40'>
+        <MovieList data={movies} title='Trending Now' />
+        <MovieList data={favorites} title='My List' />
+      </div>
     </>
   );
 }
